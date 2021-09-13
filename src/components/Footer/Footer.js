b20b7fa59420fb3f4cc313e1397cc69a19512bce
abdul riyaz <45689131+abdulriiyaz@users.noreply.github.com>
2021-09-14 01:58:30 +0530

    changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,11 +3,10 @@ import './Footer.scss'
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <div>Movie App</div>
-            <div>2021, Movie, Inc. or its affiliated</div>
-        </div>
-    )
+      <div className="footer">
+        <div>&copy;2021, Movie, Inc. or its affiliated</div>
+      </div>
+    );
 }
 
 export default Footer
